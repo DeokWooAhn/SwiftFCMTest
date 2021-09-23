42f32b7d773d7cee93446717f8f8ec72e4eb3084
fjrlcks <fjrlvkdlxj@naver.com>
2021-09-23 14:55:42 +0900

    Initial Commit

---
+++ FCM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FCM
+//
+//  Created by reborn-m1macmini1 on 2021/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
